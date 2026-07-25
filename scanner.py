64dc79f7954b68f7ea14d912839b61724c24a177
ericdurkee <eduericdurkee@gmail.com>
2026-07-25 18:21:51 -0500

    starter code + requirements.txt addition

---
+++ scanner.py
@@ -0,0 +1,9 @@
+def main():
+    print("\n=================================")
+    print("Cloud Security Scanner v0.1")
+    print("=================================")
+    print("\nInitializing...")
+    print("Setup complete!")
+
+if __name__ == "__main__":
+    main()
